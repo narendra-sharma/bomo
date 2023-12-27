e47155cdae5c7eb4c01c7dbc42d38e4a4f5f5867
Geeta <visionvivante@gmail.com>
2023-12-26 19:30:37 -0500

    designrequeststatus26-12-2023

--- src/Customer/NewRequest.js
+++ src/Customer/NewRequest.js
@@ -3,8 +3,140 @@ import React from "react";
 const NewRequest = () => {
     return (
         <>
-           <div className="App">
-                <h2>NewRequest</h2>
+           <div className="ml-md-auto pt-4 ms-md-auto rightside-wrapper">
+               <div className="main-content-wraaper px-60 py-md-2 py-lg-5">
+                 <div className="review-main-content text-center mb-4">
+                    <h3>New Request</h3>
+                    <p className="text-secondary">Sunday 16 Dec, 2023<span className="d-block">Barcelona, 21:43</span>  </p>
+                 </div>
+                 <div className="mt-5 new-request-form">
+                    <form>
+                        <div className="row">
+                            <div className="col-md-7">
+                                <div className="row">
+                                    <div className="col-md-5">
+                                        <div className="form-group">
+                                        <label className="ms-3 mb-1">Request Name</label>
+                                        <input type="text" className="form-control"/>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-7">
+                                        <div className="form-group">
+                                        <label className="ms-3 mb-1">Brand Profile</label>
+                                        <select type="select" className="form-control"/>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-12">
+                                        <div className="form-group">
+                                        <label className="ms-3 mb-1">Description</label>
+                                            <textarea className="form-control w-100" placeholder="
+                                            Describe the Brief for this piece. Include as much info as possible.
+                                            Tone and Style
+                                            Target Audience
+                                            Goal of the Piece
+                                            Display Platform
+                                            Duration"  ></textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-5 review-content">
+                                <div className="mb-4">
+                                    <label className="ms-3 mb-1">Request Type</label>
+                                    <div className="form-control py-3">
+                                        <div className="d-flex justify-content-between request-type mb-2">
+                                            <div className="request-list"><p className="short0ad logo">Logo</p></div>
+                                            <div className="request-list">
+                                            <p className="short0ad ">short ad</p>
+                                            </div>
+                                            <div className="request-list">
+                                            <p className="short0ad web-animation">web animation</p>
+                                            </div>
+                                            <div className="request-list">
+                                            <p className="short0ad icon">icon</p>
+                                            </div>
+                                        </div>
+
+                                        <div className="d-flex justify-content-between request-type mb-2">
+                                            <div className="request-list"><p className="short0ad typography">tyography</p></div>
+                                            <div className="request-list">
+                                            <p className="short0ad brand-element">Brand Element</p>
+                                            </div>
+                                            <div className="request-list">
+                                            <p className="short0ad intro">intro</p>
+                                            </div>
+                                            <div className="request-list">
+                                            <p className="short0ad icon">ourto</p>
+                                            </div>
+
+                                        </div>
+
+                                        <div className="d-flex justify-content-between request-type ">
+                                            <div className="request-list"><p className="short0ad transition">transition</p></div>
+                                            <div className="request-list">
+                                            <p className="short0ad ui-animation">Ui animation</p>
+                                            </div>
+                                            <div className="request-list">
+                                            <p className="short0ad loop">loop</p>
+                                            </div>
+                                            <div className="request-list">
+                                            <p className="short0ad custom">custom</p>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="">
+                                <div className="row">
+                                        <div className="col-md-6">
+                                            <div className="form-group">
+                                            <label className="ms-3 mb-1">File Type</label>
+                                            <select type="select" className="form-control"/>
+                                            </div>
+                                        </div>
+                                        <div className="col-md-6">
+                                            <div className="form-group">
+                                            <label className="ms-3 mb-1">(Size Up to 2)</label>
+                                            <select type="select" className="form-control"/>
+                                            </div>
+                                        </div>
+                                        <div className="col-md-6">
+                                            <div className="form-group">
+                                            <label className="ms-3 mb-1">Refernces</label>
+                                            <input type="text" className="form-control"/>
+                                            </div>
+                                        </div>
+                                        <div className="col-md-6">
+                                            <div className="form-group">
+                                            <label className="ms-3 mb-1">Transparency</label>
+                                            <select type="select" className="form-control"/>
+                                            </div>
+                                        </div>
+                                    
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-12 review-content">
+                                <label className="ms-3 mb-1">Upload Files</label>
+                                <input type="text" className="form-control"/>
+                                <p className="mt-3">You have created <b>5 pieces </b>this month. <br/>You can create 4 more pieces. Subscription renews on Nov 17</p>
+
+                            </div> 
+                            <div className="col-md-12 mt-5 pt-5 text-center status-btn ">
+                                <button type="submit" className="btn border rounded-pill pause-btn w-25 py-2">Submit</button>
+                            </div>                  
+                        </div>
+                    </form>
+                 </div>
+                
+              </div>
+              <div className="bg-gray-dark py-5">  
+                <div className="d-flex justify-content-center align-items-center">
+                    <p className="text-dark"><b>Not ready yet? </b><span className="d-block">Draft it and finish later</span></p>
+                    <button type="btn" className ="py-1 px-4  border bg-white ms-3 rounded-pill"> Save as a <span className="fw-bold">Draft</span></button>              </div>
+
+                </div>
+             
             </div>
         </>
     )
--- src/Customer/RequestStatus.js
+++ src/Customer/RequestStatus.js
@@ -3,8 +3,87 @@ import React from "react";
 const RequestStatus = () => {
     return (
         <>
-           <div className="App">
-                <h2>RequestStatus</h2>
+            <div className="ml-md-auto py-4 ms-md-auto rightside-wrapper">
+                <div className="main-content-wraaper px-60 py-md-2 py-lg-5">
+                    <div className="mx-md-2 mx-lg-5 px-60 ">
+                        <div className="order-completed px-5 py-4 rounded mb-5">
+                          <p className="mb-0">Order completed 
+                            <span className="d-block fw-bold">All good! You approved this order and files are ready to be used</span></p>
+                        </div>
+                        <div className="bg-white px-5 py-4 review-main-content rounded">
+                            <h3>Transition Brand Video</h3>
+                            <div className="review-content mt-3">
+                                <div className="d-flex">
+                                    <p className="short0ad transition">Transition</p>
+                                    <p className="short0ad transition rounded-pill ms-2">DOR</p>
+                                    <p className="short0ad project-assets ms-2">Project Assets</p>
+                                </div>
+                                <div className="delivery-date text-end">
+                                    <p className="fw-bold">Delivered on<span className="d-block fw-medium">Jan 20th 17:22</span></p>
+
+                                </div>
+
+                                <div className="table-responsive">
+                                    <table className="table table-borderless mb-0">
+                                        <thead>
+                                           <tr>
+                                                <th>Description</th>
+                                                <th>Size</th>
+                                                <th>File Type </th>
+                                                <th>Transparency</th>
+                                                <th>References</th>
+                                            </tr>
+                                         </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>Who is your target audience? <span className="d-block">What do you want to achieve with this animation?</span> 
+                                                <span className="d-block">Where is this going to appear?</span></td>
+                                                <td>9:16<span className="d-block">1:1</span></td>
+                                                <td> .mp4</td>
+                                                <td>No</td>
+                                                <td>-</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div className="delivery-status-section p-4 rounded mt-4">
+                                    <div className="row justify-content-center align-item-center">
+                                        <div className="col-md-4">
+                                            <div className="delivery-status fw-bold mb-2">
+                                               Delivery Accepted
+                                            </div>
+                                            <p className="status-date text-secondary">
+                                               17/03/2023 14:11
+                                            </p>
+                                        </div>
+                                        <div className="col-md-4 text-center">
+                                            <div className="statusbar-section">
+                                                <div className="delivery-status fw-bold">9:16</div>
+                                                <div className="bar-code"></div>
+                                                <div className="download-btn">
+                                                    <button className="rounded-pill border px-3 py-1">Download</button>
+                                                </div>
+
+                                            </div>
+                                        </div>
+                                        <div className="col-md-4 text-center">
+                                            <div className="statusbar-section">
+                                                <div className="delivery-status fw-bold">9:16</div>
+                                                <div className="bar-code"></div>
+                                                <div className="download-btn">
+                                                    <button className="rounded-pill border px-3 py-1">Download</button>
+                                                </div>
+
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
