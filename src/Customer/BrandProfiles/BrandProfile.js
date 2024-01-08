145886335ee40c0358bd9d76f6caabac796fb8c2
taranvisionvivate <taranjeetvisionvivante@gmail.com>
2024-01-08 17:34:44 +0530

    bomo_today_project

--- src/Customer/BrandProfiles/BrandProfile.js
+++ src/Customer/BrandProfiles/BrandProfile.js
@@ -1,20 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { connect, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import { deleteBrand, getbrandlist} from "../../reduxdata/rootAction";
+import { deleteBrand, getbrandlist } from "../../reduxdata/rootAction";
 import EditBrand from "../../Modals/EditBrand";
 import ManageBrand from "./ManageBrand";
 import CustomPagination from "../../Common/CustomPagination";
 import DeleteBrand from "../../Modals/Delete";
+import { format } from "date-fns";
 
 const { REACT_APP_BOMO_URL } = process.env;
 const LOGO_URL = REACT_APP_BOMO_URL;
 
-const BrandProfile = ({ brands,total,user,getbrandlist }) => {
+const BrandProfile = ({ brands, total, user, getbrandlist }) => {
 
   const dispatch = useDispatch();
   const [handleshow, setHandleshow] = useState(false);
-  const newBrand={
+  const newBrand = {
     id: '',
     logo: '',
     brandname: '',
@@ -22,12 +23,13 @@ const BrandProfile = ({ brands,total,user,getbrandlist }) => {
     tags: [],
   };
   const [edit, setEdit] = useState(newBrand);
-  const [isEdit,setIsEdit]=useState(false);
+  const [isEdit, setIsEdit] = useState(false);
   const [page, setPage] = useState(1);
   const [limit, setLimit] = useState(10);
+
   const handleShowEditBrand = (brand) => {
     setEdit({
-      id:brand._id,
+      id: brand._id,
       logo: brand.logo,
       brandname: brand.brandname,
       brandassests: brand.brandassests,
@@ -35,12 +37,12 @@ const BrandProfile = ({ brands,total,user,getbrandlist }) => {
     });
     setIsEdit(true);
   }
-  useEffect(()=>{
+  useEffect(() => {
     getbrandlist(dispatch, user.token, page, limit);
-  },[dispatch, getbrandlist, user.token, page, limit]);
+  }, [dispatch, getbrandlist, user.token, page, limit]);
 
   const [show, setShow] = useState(false);
-  const [brandid,setBrandid]=useState(null)
+  const [brandid, setBrandid] = useState(null);
   const usertoken = user.token;
   const handleDeleteBrand = (brand) => {
     setBrandid(brand?._id);
@@ -49,6 +51,7 @@ const BrandProfile = ({ brands,total,user,getbrandlist }) => {
   };
   const handleDeleteConfirm = () => {
     deleteBrand(brandid, dispatch, usertoken);
+    setEdit(newBrand);
     setShow(false);
   }
   const closeBrand = () => {
@@ -64,42 +67,44 @@ const BrandProfile = ({ brands,total,user,getbrandlist }) => {
           <div className="review-main-content mx-md-3 mx-lg-5 mb-4">
             <h3>Brand Profile</h3>
           </div>
-          
-            {brands.map((brand) => (
-              <div className="table-responsive brand-table bg-white rounded">
+
+          {brands.map((brand) => (
+            <div className="table-responsive brand-table bg-white rounded">
               <table className="table table-borderless mb-0" key={brand?._id}>
-              
+
                 <tbody>
                   <tr>
                     <td><img src={`${LOGO_URL}${brand?.logo}`} alt='img not found' style={{ height: '3rem' }} /></td>
                     <td><span className="fw-bold">Brand Name</span> <span className="d-block">{brand?.brandname}</span></td>
                     <td><span className="fw-bold">Brand Assets</span> <span className="d-block">{brand?.brandassests}</span></td>
-                    <td><span className="fw-bold">Tags</span> <span className="d-block">{brand?.tags.join(', ')}</span></td>
+                    <td>
+                      <span className="fw-bold">Date Created</span> <span className="d-block">{format(new Date(brand?.createdAt), 'MM/dd/yyyy')}</span>
+                      <span className="fw-bold">Tags</span> <span className="d-block">{brand?.tags.join(', ')}</span>
+                    </td>
                     <td >
-                      <Link style={{ textDecoration: "none" }} className="text-dark" onClick={() =>{handleShowEditBrand(brand)}}>+ edit</Link>
-                      <button  onClick={() =>{handleDeleteBrand(brand)}}>Delete</button>
+                      <Link style={{ textDecoration: "none" }} className="text-dark" onClick={() => { handleShowEditBrand(brand) }}>+ edit</Link>
+                      <button onClick={() => { handleDeleteBrand(brand) }}>Delete</button>
                     </td>
                   </tr>
                 </tbody>
               </table>
-              </div>
-            ))}
-            {(total>0) && <CustomPagination total={total} onPageChange={(newPage, newLimit) => {
-                setPage(newPage);
-                setLimit(newLimit+1);
-              }} />}
-            {
-              !handleshow ?
-                <><div className="add-new-brand"><button className="add-btn" onClick={()=>setHandleshow(true)}>+</button> <span className="ms-4 ps-2"><span className="fw-bold">Add</span> New Brand</span></div></>
-                :<>
-                    {/* <h6>Add New Brand</h6> */}
-                    <ManageBrand checkedit={isEdit} brand={edit} close={() => closeBrand()}/>
-                  </>
-            }
-            {isEdit && <EditBrand show={isEdit} handleClose={()=>closeBrand()} brand={edit} />}
-            <DeleteBrand heading='Brand Profile' name={edit.brandname} show={show} handleClose={() => setShow(false) && setEdit(newBrand)} DeleteBrand={() => handleDeleteConfirm()}/>
-          </div>
+            </div>
+          ))}
+          {(total > 0) && <CustomPagination total={total} onPageChange={(newPage, newLimit) => {
+            setPage(newPage);
+            setLimit(newLimit + 1);
+          }} />}
+          {
+            !handleshow ?
+              <><div className="add-new-brand"><button className="add-btn" onClick={() => setHandleshow(true)}>+</button> <span className="ms-4 ps-2"><span className="fw-bold">Add</span> New Brand</span></div></>
+              : <>
+                <ManageBrand checkedit={isEdit} brand={edit} close={() => closeBrand()} />
+              </>
+          }
+          {isEdit && <EditBrand show={isEdit} handleClose={() => closeBrand()} brand={edit} />}
+          <DeleteBrand heading='Brand Profile' name={edit.brandname} show={show} handleClose={() => setShow(false) && setEdit(newBrand)} DeleteBrand={() => handleDeleteConfirm()} />
         </div>
+      </div>
 
     </>
   )
@@ -117,4 +122,4 @@ const mapDispatchToProps = () => {
     getbrandlist
   };
 };
-export default connect(mapStateToProps,mapDispatchToProps)(BrandProfile);
+export default connect(mapStateToProps, mapDispatchToProps)(BrandProfile);
