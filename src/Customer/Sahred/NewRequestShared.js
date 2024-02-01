e0f0a6522ab32156e0693b3c7ff2f0cdd77678b8
narendra-sharma <devexpert2016@gmail.com>
2024-02-01 18:13:37 +0530

    Update NewRequestShared.js

--- src/Customer/Sahred/NewRequestShared.js
+++ src/Customer/Sahred/NewRequestShared.js
@@ -23,7 +23,7 @@ const NewRequestShared = ({ user }) => {
       return daysDifference;
     }
   return (
-    ((isSubscribe) || (getGraceTime()>0)) && user?.quantity && <div className="d-flex justify-content-md-end">
+    ((isSubscribe) && (user?.subscription?.status!=='paused') && user?.quantity) && <div className="d-flex justify-content-md-end">
       <div className="request-content d-flex align-items-center bg-white rounded-pill px-3 py-2 mb-4 mb-md-0">
         <Link to="/new-request" className="new-request rounded-pill px-4 py-2 fw-bold text-decoration-none text-dark">New Request</Link>
         <div className="request-date ms-2"><p className="mb-0"><span>{currentTime}</span><span className="d-block">{format(now, 'EEE dd MMM, yyyy')} </span></p></div>
