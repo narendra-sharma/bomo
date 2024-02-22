f8808fd1b5ccde022e8674d06aa647cc6f69b693
manyavv <ajayidefcoinfotec@gmail.com>
2024-02-22 15:00:23 +0530

    customer subscription

--- src/Customer/Sahred/SubscriptionStatus.js
+++ src/Customer/Sahred/SubscriptionStatus.js
@@ -4,47 +4,52 @@ import CancelSubscription from "../../Modals/CancelSubscription";
 import FinalCancel from "../../Modals/FinalCancel";
 import { isSubscription } from "../../reduxdata/rootAction";
 
-const SubscriptionStatus = ({user,isSetting}) => {
-  const [pause,setPause]=useState(false);
-  const [cancel,setCancel]=useState(false);
-  const [final,setFinal]=useState(false);
-  const [isSubscribe,setIsSubscribe]=useState(false);
-    const getSubscription=async()=>{
-      await isSubscription(user).then(r=>{
-        setIsSubscribe(r);
-      });
-    }
-    useEffect(()=>{
-      getSubscription();
-    },[]);
-  const Status=()=>{
+const SubscriptionStatus = ({ user, isSetting, subscribedUser }) => {
+  let getSubscriptionId = subscribedUser?.subscription?.filter((item) => item.type == 'primary');
+  console.log("NEWWWW USERRRRRRRR", user);
+  const [pause, setPause] = useState(false);
+  const [cancel, setCancel] = useState(false);
+  const [final, setFinal] = useState(false);
+  const [isSubscribe, setIsSubscribe] = useState(false);
+  const getSubscription = async () => {
+    await isSubscription(user).then(r => {
+      setIsSubscribe(r);
+    });
+  }
+  useEffect(() => {
+    getSubscription();
+  }, []);
+  const Status = () => {
     return <>
       <b>{isSetting && 'Subscription'} Status</b>
-      <span className={`d-block  ${(isSubscribe)?(user?.subscription?.status==='active'?'ACTIVE':'INACTIVE'):(user?.next_billing_date?'EXPIRED':'INACTIVE')}`}>{(isSubscribe)?(user?.subscription?.status==='active'?'ACTIVE':'INACTIVE'):(user?.next_billing_date?'EXPIRED':'INACTIVE')}</span>
+      {
+        !subscribedUser ? <span className={`d-block  ${(isSubscribe) ? ((user?.subscription?.status) === 'active' ? 'ACTIVE' : 'INACTIVE') : (user?.next_billing_date ? 'EXPIRED' : 'INACTIVE')}`}>{(isSubscribe) ? (user?.subscription?.status === 'active' ? 'ACTIVE' : 'INACTIVE') : (user?.next_billing_date ? 'EXPIRED' : 'INACTIVE')}</span> :
+          <span className={`d-block  ${(isSubscribe) ? ((user?.subscription?.status) === 'active' ? 'ACTIVE' : 'INACTIVE') : (user?.next_billing_date ? 'EXPIRED' : 'INACTIVE')}`}>{(isSubscribe) ? (getSubscriptionId && getSubscriptionId[0]?.status === 'active' ? 'ACTIVE' : 'INACTIVE') : (user?.next_billing_date ? 'EXPIRED' : 'INACTIVE')}</span>
+      }
     </>
   }
   return (
-    <div className={`review-content bg-white rounded px-4  ${isSetting?'px-md-4 py-4':'px-md-5 py-5'}`}>
+    <div className={`review-content bg-white rounded px-4  ${isSetting ? 'px-md-4 py-4' : 'px-md-5 py-5'}`}>
       <div className="row">
         <div className="col-lg-6 col-6">
-          {isSetting?
-          <h6 className="position-relative ps-sm-0 ps-2 mb-0">
-            <Status/>
-          </h6>
-          :<p className="status subscription-status position-relative ps-sm-0 ps-2">
-            <Status/>
-          </p>}
+          {isSetting ?
+            <h6 className="position-relative ps-sm-0 ps-2 mb-0">
+              <Status />
+            </h6>
+            : <p className="status subscription-status position-relative ps-sm-0 ps-2">
+              <Status />
+            </p>}
         </div>
-        {isSubscribe && (user?.subscription?.status !=='inactive') && <div className={`col-lg-6  ${isSetting && 'text-end'}`}>
+        {isSubscribe && (user?.subscription?.status !== 'inactive') && <div className={`col-lg-6  ${isSetting && 'text-end'}`}>
           <div className="status-btn d-flex justify-content-end">
-            <button className="btn border rounded-pill pause-btn" onClick={()=>setPause(true)}>{user?.subscription?.status==='paused'?'RESUME':'PAUSE'} </button>
-            {(user?.subscription?.status ==='active') && <button className="btn border rounded-pill cancel-btn" onClick={()=>setCancel(true)}>CANCEL</button>}
+            {!subscribedUser ? <button className="btn border rounded-pill pause-btn" onClick={() => setPause(true)}>{user?.subscription?.status === 'paused' ? 'RESUME' : 'PAUSE'} </button> : <button className="btn border rounded-pill pause-btn" onClick={() => setPause(true)}>{getSubscriptionId && getSubscriptionId[0]?.status == 'active' ? 'Pause' : "Resume"}</button>}
+            {(user?.subscription?.status === 'active') && <button className="btn border rounded-pill cancel-btn" onClick={() => setCancel(true)}>CANCEL</button>}
           </div>
         </div>}
       </div>
-      <PauseSubscription show={pause} handleClose={() => setPause(false)} />
-      <CancelSubscription show={cancel} handleClose={() => setCancel(false)} final={()=>setFinal(true)} pause={()=>setPause(true)}/>
-      <FinalCancel show={final} handleClose={() => setFinal(false)} />
+      <PauseSubscription show={pause} handleClose={() => setPause(false)} user={user} subscribedUser={subscribedUser} />
+      <CancelSubscription show={cancel} handleClose={() => setCancel(false)} final={() => setFinal(true)} pause={() => setPause(true)} />
+      <FinalCancel show={final} handleClose={() => setFinal(false)} user={user} />
     </div>
   )
 }
--- src/reduxdata/PlansPayments/planReducer.js
+++ src/reduxdata/PlansPayments/planReducer.js
@@ -1,39 +1,47 @@
-import { CUSTOMER_CARD, GET_PAYMENT_HISTORY, GET_PLANS, PAY_NOW } from "./planTypes";
+import { CUSTOMER_CARD, GET_PAYMENT_HISTORY, GET_PLANS, PAY_NOW, SUBSCRIPTION_INFO } from "./planTypes";
 
 const initialState = {
-    plans: [],
-    isPay:false,
-    payments:[],
-    cpayments:[],
-    dpayments:[],
-    total:0,
-    cards:null
-  };
-  
-  const planReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case GET_PLANS:
-        return { ...state, 
-          plans: action.payload?.data[0]?.tiers
-        };
-      case PAY_NOW:
-        return { ...state, 
-          isPay: !state.isPay 
-        }; 
-      case GET_PAYMENT_HISTORY:
-        return {
-          ...state,
-          payments:action.payload?.data,
-          total:action.payload?.total
-        }  
-      case CUSTOMER_CARD:
+  plans: [],
+  isPay: false,
+  payments: [],
+  cpayments: [],
+  dpayments: [],
+  subscription: [],
+  total: 0,
+  cards: null
+};
+
+const planReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_PLANS:
       return {
         ...state,
-        cards:action.payload
-      }   
-      default:
-        return state;
-    }
-  };
-  
-  export default planReducer;
+        plans: action.payload?.data[0]?.tiers
+      };
+    case PAY_NOW:
+      return {
+        ...state,
+        isPay: !state.isPay
+      };
+    case GET_PAYMENT_HISTORY:
+      return {
+        ...state,
+        payments: action.payload?.data,
+        total: action.payload?.total
+      }
+    case CUSTOMER_CARD:
+      return {
+        ...state,
+        cards: action.payload
+      }
+    case SUBSCRIPTION_INFO:
+      return {
+        ...state,
+        subscription: action.payload
+      }
+    default:
+      return state;
+  }
+};
+
+export default planReducer;
