dbd6edbe76cb0545288529a1676d5316601ab6a8
manyavv <ajayidefcoinfotec@gmail.com>
2024-03-01 15:20:00 +0530

    brand profile and super admin fixes

--- src/Customer/BrandProfiles/BrandProfile.js
+++ src/Customer/BrandProfiles/BrandProfile.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
 import { connect, useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, useRevalidator } from "react-router-dom";
 import { addBrand, deleteBrand, getbrandlist, uploadZip } from "../../reduxdata/rootAction";
 import ManageBrand from "./ManageBrand";
 import CustomPagination from "../../Common/CustomPagination";
@@ -15,7 +15,6 @@ const LOGO_URL = REACT_APP_BOMO_URL;
 
 const BrandProfile = ({ brands, total, user, zipfile_path }) => {
   const dispatch = useDispatch();
-  console.log("USERRRRRRRRR", user);
   const usertoken = user.token;
   const [handleshow, setHandleshow] = useState(false);
   const [imagePreview, setImagePreview] = useState('');
@@ -192,7 +191,7 @@ const BrandProfile = ({ brands, total, user, zipfile_path }) => {
       <div className="ml-md-auto py-4 ms-md-auto rightside-wrapper">
         <div className="main-content-wraaper brand-profile-section px-60 py-md-2 py-lg-5">
           <div className="mx-md-3 mx-lg-5 mb-4">
-          {((user?.role === 'customer_admin')) && <SharedRequest />}
+            {((user?.role === 'customer_admin')) && <SharedRequest />}
           </div>
           <div className="review-main-content mx-md-3 mx-lg-5 mb-4">
             <h3>Brand Profile</h3>
@@ -266,11 +265,18 @@ const BrandProfile = ({ brands, total, user, zipfile_path }) => {
                             </>}
                           </span>
                           <span className="edit gap-5 d-flex align-items-center justify-content-center">
-                            {console.log("ASDASDASDASDASDDASDASDAS", user?.role)}
                             {(isEdit && edit?.id === brand?._id) ?
                               <Link className="text-dark text-decoration-none" onClick={() => { handleExit(); handleShowEditBrand(newBrand); }}><span className="fa fa-times"></span></Link>
                               :
-                              user?.role !== "customer" && <Link className="text-dark text-decoration-none" onClick={() => { handleShowEditBrand(brand); setHandleshow(false); }}>+ edit</Link>
+                              user?.role !== "customer_admin" && user?._id == brand?.user_id &&
+                              <Link className="text-dark text-decoration-none" onClick={() => { handleShowEditBrand(brand); setHandleshow(false); }}>+ edit</Link>
+
+                            }
+                            {user?.role == "customer_admin" &&
+                              (isEdit && edit?.id === brand?._id) ?
+                              <Link className="text-dark text-decoration-none" onClick={() => { handleExit(); handleShowEditBrand(newBrand); }}><span className="fa fa-times"></span></Link>
+                              :
+                              user?.role == "customer_admin" && <Link className="text-dark text-decoration-none" onClick={() => { handleShowEditBrand(brand); setHandleshow(false); }}>+ edit</Link>
                             }
                           </span>
                         </div>
