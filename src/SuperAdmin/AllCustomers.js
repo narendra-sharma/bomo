44b9dd1b96e034af87da00f963955d704a52dbe1
manyavv <ajayidefcoinfotec@gmail.com>
2024-03-13 16:59:01 +0530

    some issues related to switch

--- src/SuperAdmin/AllCustomers.js
+++ src/SuperAdmin/AllCustomers.js
@@ -7,9 +7,11 @@ import CustomPagination from "../Common/CustomPagination";
 import { format } from "date-fns";
 import ViewAsCustomer from "../Modals/ViewAsCustomer";
 import EmptyList from "../Common/EmptyList";
+import { useNavigate } from "react-router-dom";
 
 const AllCustomers = ({ user, users, total }) => {
   const dispatch = useDispatch();
+  const naviagte = useNavigate();
   const role = "customer_admin";
   const [search, setSearch] = useState(null);
   const [view, setView] = useState(null);
@@ -19,87 +21,109 @@ const AllCustomers = ({ user, users, total }) => {
   }, [search]);
 
   const handleswitchto_designer = async (userId) => {
-    await switch_to_designer(dispatch,userId,user?.token);
+    await switch_to_designer(dispatch, userId, user?.token, naviagte);
   };
 
   return (
     <>
       <div className="ml-md-auto py-4 ms-md-auto rightside-wrapper">
         <div className="main-content-wraaper admin-payments customer-all px-60 py-md-2 py-lg-3">
-          <h3 className="fw-bold mb-3 counter-circle d-flex align-items-center gap-2">{total > 0 && <span className="rounded-circle bg-white mr-2">{total}</span>} Customers</h3>
+          <h3 className="fw-bold mb-3 counter-circle d-flex align-items-center gap-2">
+            {total > 0 && (
+              <span className="rounded-circle bg-white mr-2">{total}</span>
+            )}{" "}
+            Customers
+          </h3>
           <SearchInput
             placeholder="Browse Customers..."
             handleSearch={(val) => setSearch(val)}
           />
           <div className="mt-5 review-main-content">
             <div className="review-content mb-4">
-            <div className="table-responsive" >
-              <table className="table table-borderless mb-0">
+              <div className="table-responsive">
+                <table className="table table-borderless mb-0">
                   <tbody>
                     {users?.length > 0 ? (
                       users.map((item) => (
-                      
-                              <tr key={item?._id}>
-                                <td>
-                                  <p>
-                                    <button
-                                      className="rounded-pill rounded-pill py-1 px-2 btn btn-outline-dark"
-                                      onClick={() => handleswitchto_designer(item?._id)}
-                                    >
-                                      View as customer
-                                    </button>
-                                  </p>
-                                </td>
-                                <td>
-                                  <p className="fw-bold">{item?.company}</p>
-                                </td>
-                                <td>
-                                  <p className="fw-bold color-light-blue">
-                                    {item?.active_requests} ACTIVE Requests
-                                  </p>
-                                </td>
-                                <td>
-                                  <p>
-                                    <span className="fw-bold">Date added</span>{" "}
-                                    <span className="d-block">
-                                      {item?.createdAt &&
-                                        format(
-                                          new Date(item?.createdAt),
-                                          "dd/MM/yyyy"
-                                        )}
-                                    </span>
-                                  </p>
-                                </td>
-                                <td>
-                                  <p>
-                                    <span className="fw-bold">Contact </span>{" "}
-                                    <span className="d-block">{item?.email}</span>
-                                  </p>
-                                </td>
-                                <td>
-                                  <p>
-                                    <span className="fw-bold">Monthly Requests</span>{" "}
-                                    <span className="d-block">{item?.monthly_request ? item?.monthly_request : '0'}</span>
-                                  </p>
-                                </td>
-                                <td>
-                                  <p>
-                                    <span className="fw-bold">Total Admins</span>{" "}
-                                    <span className="d-block">{item?.no_of_admin}</span>
-                                  </p>
-                                </td>
-                                <td>
-                                  <p>
-                                    <span className="fw-bold">Members</span>{" "}
-                                    <span className="d-block">{item?.no_of_cust}</span>
-                                  </p>
-                                </td>
-                                <td>
-                                  <p>
-                                    <span className="cursor-pointer" onClick={() => { setView(item); setShow(true); }}>expand</span>
-                                  </p>
-                                </td>
-                              </tr>      
+                        <tr key={item?._id}>
+                          <td>
+                            <p>
+                              <button
+                                className="rounded-pill rounded-pill py-1 px-2 btn btn-outline-dark"
+                                onClick={() =>
+                                  handleswitchto_designer(item?._id)
+                                }
+                              >
+                                View as customer
+                              </button>
+                            </p>
+                          </td>
+                          <td>
+                            <p className="fw-bold">{item?.company}</p>
+                          </td>
+                          <td>
+                            <p className="fw-bold color-light-blue">
+                              {item?.active_requests} ACTIVE Requests
+                            </p>
+                          </td>
+                          <td>
+                            <p>
+                              <span className="fw-bold">Date added</span>{" "}
+                              <span className="d-block">
+                                {item?.createdAt &&
+                                  format(
+                                    new Date(item?.createdAt),
+                                    "dd/MM/yyyy"
+                                  )}
+                              </span>
+                            </p>
+                          </td>
+                          <td>
+                            <p>
+                              <span className="fw-bold">Contact </span>{" "}
+                              <span className="d-block">{item?.email}</span>
+                            </p>
+                          </td>
+                          <td>
+                            <p>
+                              <span className="fw-bold">Monthly Requests</span>{" "}
+                              <span className="d-block">
+                                {item?.monthly_request
+                                  ? item?.monthly_request
+                                  : "0"}
+                              </span>
+                            </p>
+                          </td>
+                          <td>
+                            <p>
+                              <span className="fw-bold">Total Admins</span>{" "}
+                              <span className="d-block">
+                                {item?.no_of_admin}
+                              </span>
+                            </p>
+                          </td>
+                          <td>
+                            <p>
+                              <span className="fw-bold">Members</span>{" "}
+                              <span className="d-block">
+                                {item?.no_of_cust}
+                              </span>
+                            </p>
+                          </td>
+                          <td>
+                            <p>
+                              <span
+                                className="cursor-pointer"
+                                onClick={() => {
+                                  setView(item);
+                                  setShow(true);
+                                }}
+                              >
+                                expand
+                              </span>
+                            </p>
+                          </td>
+                        </tr>
                       ))
                     ) : (
                       <EmptyList name="Customers" />
