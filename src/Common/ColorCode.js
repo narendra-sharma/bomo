2e9e2c9fb13dfc614582be5707c556a7cc9efc8b
manyavv <ajayidefcoinfotec@gmail.com>
2024-03-13 18:59:16 +0530

    added brand logo in expand request

--- src/Common/ColorCode.js
+++ src/Common/ColorCode.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { connect } from "react-redux";
 
 const ColorCode = ({ request, requestTypes, reqtype }) => {
-
   const [isHovered, setIsHovered] = useState(null);
 
   const handleMouseOver = (requestid) => {
@@ -15,36 +14,38 @@ const ColorCode = ({ request, requestTypes, reqtype }) => {
 
   const findColorByType = (type) => {
     const lowerCaseType = type?.toLowerCase();
-    const foundtype = requestTypes?.find((t) => t.type?.toLowerCase() === lowerCaseType);
-    return foundtype ? foundtype?.color : '';
+    const foundtype = requestTypes?.find(
+      (t) => t.type?.toLowerCase() === lowerCaseType
+    );
+    return foundtype ? foundtype?.color : "";
   };
-
+  console.log("Check");
   return (
     <>
-        <p
-          className={`short0ad ${findColorByType(
+      <p
+        className={`short0ad ${findColorByType(
+          request?.request_type?.replace(/_/g, " ")
+        )}`}
+        style={{
+          color: reqtype
+            ? "white"
+            : isHovered === request?._id
+            ? "white"
+            : findColorByType(request?.request_type?.replace(/_/g, " ")),
+          background: reqtype
+            ? findColorByType(request?.request_type?.replace(/_/g, " "))
+            : isHovered === request?._id
+            ? findColorByType(request?.request_type?.replace(/_/g, " "))
+            : "white",
+          border: `1px solid ${findColorByType(
             request?.request_type?.replace(/_/g, " ")
-          )}`}
-          style={{
-            color:
-              reqtype ? "white" :
-              isHovered === request?._id
-                ? "white"
-                : findColorByType(request?.request_type?.replace(/_/g, " ")),
-            background: 
-            reqtype ? findColorByType(request?.request_type?.replace(/_/g, " ")) :
-              isHovered === request?._id
-                ? findColorByType(request?.request_type?.replace(/_/g, " "))
-                : "white",
-            border: `1px solid ${findColorByType(
-              request?.request_type?.replace(/_/g, " ")
-            )}`,
-          }}
-          onMouseEnter={() => handleMouseOver(request?._id)}
-          onMouseLeave={handleMouseOut}
-        >
-          {request?.request_type?.replace(/_/g, " ")}
-        </p>
+          )}`,
+        }}
+        onMouseEnter={() => handleMouseOver(request?._id)}
+        onMouseLeave={handleMouseOut}
+      >
+        {request?.request_type?.replace(/_/g, " ")}
+      </p>
     </>
   );
 };
@@ -52,8 +53,7 @@ const ColorCode = ({ request, requestTypes, reqtype }) => {
 const mapStateToProps = (state) => {
   return {
     requestTypes: state.requests.requestTypes,
-  }
-}
+  };
+};
 
 export default connect(mapStateToProps)(ColorCode);
-
